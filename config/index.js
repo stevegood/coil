b1f3661af7a85482838c74ed45526f35e185eb6a
Steve Good <steve.good@am.sony.com>
2017-02-13 11:41:16 -0800

    Initial commit

---
+++ config/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  user: 'stevegood',
+  api_key: '586f21e09580b07a5694e488db7cf903'
+}
---
+++ main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
+
+let win
+
+const createWindow = (win) => {
+  win = new BrowserWindow({
+    width: 174,
+    height: 174,
+    frame: false
+  })
+
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  win.on('closed', () => {
+    win = null
+  })
+
+  return win
+}
+
+app.on('ready', () => {
+  win = createWindow(win)
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (!win) {
+    win = createWindow(win)
+  }
+})
